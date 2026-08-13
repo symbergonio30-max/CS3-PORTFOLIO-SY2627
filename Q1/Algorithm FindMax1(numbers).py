2fad15eae6851566f7fa73f8c04bd2bfd5f16489
symbergonio30-max <symbergonio30@src.pshs.edu.ph>
2026-08-13 16:04:36 +0800

    Add files via upload

---
+++ Q1/Algorithm FindMax1(numbers).py
@@ -0,0 +1,17 @@
+Algorithm FindMax1(numbers)
+
+   max ← numbers[0]
+
+   For i from 1 to length(numbers)-1
+
+      If numbers[i] > max Then
+
+         max ← numbers[i]
+
+      EndIf
+
+   EndFor
+
+   Return max
+
+EndAlgorithm
